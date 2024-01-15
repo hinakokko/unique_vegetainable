fb6b6974ec9f3d4c2bf121689033337d756b9d33
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2024-01-15 13:30:38 +0000

    240115_2230_Bootstrapエラー解決

--- app/models/vegetainable_review.rb
+++ app/models/vegetainable_review.rb
@@ -1,5 +1,8 @@
 class VegetainableReview < ApplicationRecord
 
   has_one_attached :image
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
 end
