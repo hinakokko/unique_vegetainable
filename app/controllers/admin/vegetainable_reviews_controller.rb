b349b6b0a857a26beadf3bf17a85ac0bc948afae
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2024-01-22 12:06:38 +0000

    240122_2106_会員のユーザー管理

--- app/controllers/admin/vegetainable_reviews_controller.rb
+++ app/controllers/admin/vegetainable_reviews_controller.rb
@@ -5,8 +5,15 @@ class Admin::VegetainableReviewsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @vegetainable_review = @user.vegetainable_reviews.find(params[:id])
+    # @user = User.find(params[:user_id])
+    @vegetainable_review = VegetainableReview.find(params[:id])
+    @user = @vegetainable_review.user
+    # @vegetainable_review = @user.vegetainable_reviews.first
+    if @vegetainable_review.nil?
+      redirect_to admin_users_path
+    end
+    # @user_id = @user.vegetainable_reviews
+    # redirect_to admin_user_vegetainable_review_path(@user)
   end
 
 end
