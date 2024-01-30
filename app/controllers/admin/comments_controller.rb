e4118dc53e2af8ef20bbf9ad00cb05c2f25a3ad8
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2024-01-30 13:57:15 +0000

    240130_2256_デプロイ後_レイアウト調整

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -3,7 +3,7 @@ class Admin::CommentsController < ApplicationController
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
-    redirect_back fallback_location: root_path
+    redirect_back fallback_location: admin_top_path
   end
 
 end
